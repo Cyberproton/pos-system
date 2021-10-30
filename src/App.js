4fbfc4e9ae4c39089a5e0cac1987d487dcbec438
Cyberproton <cyberprotonAtMailbox@gmail.com>
2021-10-30 22:02:18 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Cart from "./pages/Cart";
+import Food from "./pages/Food";
+import Payment from "./pages/Payment";
+import _404_ from "./pages/_404_";
 
-function App() {
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/css/style.css";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header/>
+      <Switch>
+        <Route exact path={"/"} component={Food}/>
+        <Route exact path={"/cart"} component={Cart}/>
+        <Route exact path={"/payment"} component={Payment}/>
+        <Route component={_404_}/>
+      </Switch>
+      <Footer/>
+    </Router>
   );
 }
-
-export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="/">POS System</a>
+                <div className="collapse navbar-collapse" id="navbarText">
+                    <ul className="navbar-nav ms-3 me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="/">Food</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="cart">Cart</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="payment">Payment</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
